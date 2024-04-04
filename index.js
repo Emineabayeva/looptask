469b8742aea2505ecd0988ee989a878d6b876e1f
Emineabayeva <abayevaemine01@gmail.com>
2024-04-04 10:55:17 +0400

    task added

---
+++ index.js
@@ -0,0 +1,51 @@
+   
+// let eded = parseInt(prompt ("eded daxil edin"));
+// let count=0
+// let hal =0
+// for (let i = 1; i< eded; i++) {
+//     if(eded %i==0){
+//         count++
+//     }
+// }
+// if(count>2){
+//     console.log("eded murekkeb")
+// }
+//    else{
+//     console.log("eded sade")
+//    }
+   
+   
+    
+   //    task4
+
+// let arr = [1, 4, 7, 22, 15, 8];
+// let x = 0;
+// let counter = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr [i] < 10) {
+         
+//         x+= arr[i];
+//         counter++
+//     }
+// }
+
+// console.log(" ededlerin cemi:", x);
+     
+         // task1
+
+
+
+
+let number = 27;
+let isPowerOf3 = false;
+
+for (let i = 0; i <= number; i++) {
+    if (Math.pow(3, i) === number) {
+    }
+}
+
+if (isPowerOf3) {
+    console.log(number + " 3-ün quvvətidir.");
+} else {
+    console.log(number + " 3-ün quvvəti deyil.");
+}
